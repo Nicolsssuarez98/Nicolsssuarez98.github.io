6fb33212fd915f605ec93c67a61b5e072fff7ef2
Nicolsssuarez98 <lalala98teamo@gmail.com>
2017-09-09 14:53:41 -0500

    cambio cosas;

--- script/main.js
+++ script/main.js
@@ -34,11 +34,11 @@ $(document).ready(function(){
 	$('.ir-arriba').click(function(){
 		$('body, html').animate({
 			scrollTop: '0px'
-		}, 300);
+		}, 600);
 	});
  
 	$(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
+		if( $(this).scrollTop() > 400 ){
 			$('.ir-arriba').slideDown(300);
 		} else {
 			$('.ir-arriba').slideUp(300);
